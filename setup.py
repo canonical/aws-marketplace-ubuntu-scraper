b94e79f8bbbdb623ea5337d43545b367cd2855c4
Thomas Bechtold <thomas.bechtold@canonical.com>
2022-01-05 13:29:40 +0100

    Install executable as aws-marketplace-ubuntu-scraper
    Instead of using aws_marketplace_ubuntu_scraper, use
    aws-marketplace-ubuntu-scraper. Using '-' is more common for CLI
    tools.
    
    Note: "python -m aws_marketplace_ubuntu_scraper" does still work.

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'aws_marketplace_ubuntu_scraper = '
+            'aws-marketplace-ubuntu-scraper = '
             'aws_marketplace_ubuntu_scraper:main',
         ],
     },
